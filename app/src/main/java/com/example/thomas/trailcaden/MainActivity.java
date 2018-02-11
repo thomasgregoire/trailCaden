9a8a0fcd46a7f40741289bf84be7622c2fd69387
safiahseedat <safiah.seedat@gmail.com>
2018-02-11 14:13:57 +0100

    commit for pull

--- app/src/main/java/com/example/thomas/trailcaden/MainActivity.java
+++ app/src/main/java/com/example/thomas/trailcaden/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.thomas.trailcaden;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OpenCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }

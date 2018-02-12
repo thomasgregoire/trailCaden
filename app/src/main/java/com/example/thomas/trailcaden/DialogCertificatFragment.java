e5e265ca6f92e548852dbed6e60fd1ca0c6b1e10
Thomas <thomas0035@gmail.com>
2018-02-12 16:19:25 +0100

    AJout dialog avec image certificat médical

--- app/src/main/java/com/example/thomas/trailcaden/DialogCertificatFragment.java
+++ app/src/main/java/com/example/thomas/trailcaden/DialogCertificatFragment.java
@@ -5,6 +5,13 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Thomas on 12/02/2018.
@@ -14,19 +21,24 @@ public class DialogCertificatFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
+        
+        ImageView ivBasicImage = new ImageView(getActivity());
+        Picasso.with(getActivity()).load(R.mipmap.jojo).resize(500, 800).into(ivBasicImage);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("TEST")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        builder.setMessage(R.string.title_dialog)
+                .setPositiveButton(R.string.valider_dialog, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // FIRE ZE MISSILES!
                     }
                 })
-                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                .setNegativeButton(R.string.annuler_dialog, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // User cancelled the dialog
                     }
                 });
+
+        builder.setView(ivBasicImage);
         // Create the AlertDialog object and return it
         return builder.create();
     }

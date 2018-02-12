1a1f870c934caf7ba6f61732bce69421ea7911aa
safiahseedat <safiah.seedat@gmail.com>
2018-02-12 21:29:40 +0100

    Map

--- app/src/main/java/com/example/thomas/trailcaden/MapActivity.java
+++ app/src/main/java/com/example/thomas/trailcaden/MapActivity.java
@@ -7,11 +7,34 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Safiah on 12/02/2018.
  */
 
-public class MapActivity extends AppCompatActivity {
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    protected final LatLng CADEN = new LatLng(47.6333,-2.2833);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().position(CADEN).title("Caden"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CADEN));
     }
 }

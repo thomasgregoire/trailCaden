8d51764739eb03521659c3594139596ea4ef9f05
safiahseedat <safiah.seedat@gmail.com>
2018-02-12 10:13:32 +0100

    Ajout map activity

--- app/src/main/java/com/example/thomas/trailcaden/MainActivity.java
+++ app/src/main/java/com/example/thomas/trailcaden/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OpenTel(View view) {
-        String number = "06 79 54 35 91";
+        String number = "06 26 03 44 19";
         Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", number, null));
         int permissionCheck = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CALL_PHONE);
@@ -50,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
     public void testAdmin(View view){
         Intent intent = new Intent(MainActivity.this, AdminActivity.class);
 
+        startActivity(intent);
+    }
+
+    public void mapActivity(View view){
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/thomas/trailcaden/MapActivity.java
@@ -0,0 +1,17 @@
+package com.example.thomas.trailcaden;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Safiah on 12/02/2018.
+ */
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}

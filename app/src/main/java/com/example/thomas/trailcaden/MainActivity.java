a7d55fb23b27aa9a43d94e2ef90df931d335954b
kadnox <y-jos56@hotmail.fr>
2018-02-12 10:08:18 +0100

    Ajout bouton pour partie météo (Intent vers weather) pas passé au précédent commit

--- app/src/main/java/com/example/thomas/trailcaden/MainActivity.java
+++ app/src/main/java/com/example/thomas/trailcaden/MainActivity.java
@@ -52,4 +52,10 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
+
+    public void weather(View view){
+        Intent intent = new Intent(MainActivity.this, Weather.class);
+
+        startActivity(intent);
+    }
 }

dd98d48ab5356523d418b8ff2752fcf689949e29
Thomas <thomas0035@gmail.com>
2018-02-13 16:41:26 +0100

    Merge branch 'master' of https://github.com/thomasgregoire/trailCaden
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/thomas/trailcaden/admin/fragments/DialogCertificatFragment.java
+++ app/src/main/java/com/example/thomas/trailcaden/admin/fragments/DialogCertificatFragment.java
@@ -10,8 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.thomas.trailcaden.R;
+import com.example.thomas.trailcaden.model.Person;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -20,9 +28,13 @@ import com.squareup.picasso.Picasso;
 
 public class DialogCertificatFragment extends DialogFragment {
 
+    private String mail;
+
+    private DatabaseReference mDatabase;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         ImageView ivBasicImage = new ImageView(getActivity());
         Picasso.with(getActivity()).load(R.mipmap.jojo).resize(500, 800).
                 centerCrop().into(ivBasicImage);
@@ -31,12 +43,27 @@ public class DialogCertificatFragment extends DialogFragment {
         builder.setMessage(R.string.title_dialog)
                 .setPositiveButton(R.string.valider_dialog, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
+                        Query test = mDatabase.child("users").orderByChild("mail").equalTo(getMail());
+
+                        test.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for (DataSnapshot child: dataSnapshot.getChildren()) {
+
+                                    child.getRef().child("inscrit").setValue(true);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
                     }
                 })
                 .setNegativeButton(R.string.annuler_dialog, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
+
                     }
                 });
 
@@ -45,4 +72,11 @@ public class DialogCertificatFragment extends DialogFragment {
         return builder.create();
     }
 
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 }
